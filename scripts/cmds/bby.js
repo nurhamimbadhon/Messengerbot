57fda1dd6fae7910b9bf9a84d1969cd16dc68c93
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-03-30 19:46:56 +0600

    Update bby.js

--- scripts/cmds/bby.js
+++ scripts/cmds/bby.js
@@ -195,7 +195,7 @@ module.exports.onChat = async ({ api, event }) => {
   const cMessages = ["🎀 Hello bby!", "🎀 Hi there!", "🎀 Hey! How can I help?😝"];
   const userInput = body.toLowerCase().trim();
 
-  const keywords = ["bby", "shan", " faiza", "hii", "baby", "bot", "বট", "robot"];
+  const keywords = ["sona", "vixa", "Nur", "hi", "pakhi", "janu", "robot"];
 
   if (keywords.some((keyword) => userInput.startsWith(keyword))) {
     const isQuestion = userInput.split(" ").length > 1;
