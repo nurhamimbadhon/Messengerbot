572d820b0aa7c38d7515e0d564acb27bfb8b30d5
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-04-03 01:06:12 +0600

    Update fucku.js

--- scripts/cmds/fucku.js
+++ scripts/cmds/fucku.js
@@ -1,22 +1,25 @@
 module.exports = {
- config: {
-	 name: "fuck you",
-	 version: "1.0",
-	 author: "AceGun",
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "no prefix",
- },
+  config: {
+    name: "fuck you",
+    version: "1.0",
+    author: "Nur Hamim",
+    countDown: 0,
+    role: 0,
+    shortDescription: "no prefix",
+    longDescription: "no prefix",
+    category: "no prefix",
+  },
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "fuck") {
- return message.reply({
- body: "Fuck you too🖕",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/9bNeakd.gif")
- });
- }
- }
-}
+  onChat: async function ({ event, message }) {
+    if (event.body && /f[\W_]*[u@]([\W_]*[c@])?[\W_]*k[\W_]*[y@]?[o0]?[u@]?/i.test(event.body)) {
+      try {
+        return message.reply({
+          body: "Fuck you too🖕",
+          attachment: await global.utils.getStreamFromURL("https://i.imgur.com/9bNeakd.gif")
+        });
+      } catch (err) {
+        return message.reply("Fuck you too🖕");
+      }
+    }
+  }
+};
