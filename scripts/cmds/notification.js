ca1b640b3cc1e710f22a725e738f08c26a98a960
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-04-02 22:57:46 +0600

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -31,7 +31,7 @@ module.exports = {
 		},
 		en: {
 			missingMessage: "Bby ki ki pathaibo oita bolo 🐸",
-			notification: "~🗣️ NOTIFICATION FROM ADMIN Ew'r ShAn's",
+			notification: "~🗣️ Message from admin",
 			sendingNotification: "Start sending notification from admin bot to %1 chat groups",
 			sentNotification: "✅ Sent notification to %1 groups successfully",
 			errorSendingNotification: "An error occurred while sending to %1 groups:\n%2"
